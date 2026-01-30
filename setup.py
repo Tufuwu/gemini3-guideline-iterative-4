b8af88aa66156b89f96ba9cebd0712fbc9f86036
wu <wu@.com>
2026-01-30 14:24:27 +0800

    Logs-databricks/spark-deep-learning

--- setup.py
+++ setup.py
@@ -1,74 +1,45 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015 IAS / CNRS / Univ. Paris-Sud
-# BSD License - see attached LICENSE file
-# Author: Alexandre Boucaud <alexandre.boucaud@ias.u-psud.fr>
-from setuptools import setup, find_packages
+""" distribute- and pip-enabled setup.py """
 
+import setuptools
+import sys
 
-def find_version(filepath):
-    """
-    Find project version in a given file
+import sparkdl
 
-    The syntax for the file version need to be in the form
-    __version__ = 'a.b.c'
-    which follows the semantic versioning http://semver.org/
-    * a : major version
-    * b : minor version
-    * c : patch version
+exclude_packages = ('tests', 'tests.*')
 
-    Parameters
-    ----------
-    filepath: str
-        Path to the file containing a version number
+if '--with-tests' in sys.argv:
+    index = sys.argv.index('--with-tests')
+    sys.argv.pop(index)
+    exclude_packages = ()
 
-    Returns
-    -------
-    version: str
-        The program version in the form 'a.b.c' as described above
-
-    """
-    with open(filepath) as pfile:
-        for line in pfile.readlines():
-            if line.startswith('__version__'):
-                version = line.strip()[-6:-1]
-    return version
-
-setup(
-    name='pypher',
-    author='Alexandre Boucaud',
-    author_email='alexandre.boucaud@ias.u-psud.fr',
-    maintainer='Alexandre Boucaud',
-    maintainer_email='boucaud.alexandre@gmail.com',
-    description='Python-based PSF Homogenization kERnels production',
-    license='New BSD',
-    url='http://pypher.readthedocs.org/en/latest/',
-    download_url='https://git.ias.u-psud.fr/aboucaud/pypher/',
-    version=find_version('pypher/pypher.py'),
-    long_description=open('README.rst').read(),
-    zip_safe=False,
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points={
-        'console_scripts': [
-            'pypher = pypher.pypher:main',
-            'addpixscl = pypher.addpixscl:main',
-        ],
-    },
-    install_requires=[
-        'numpy>=1.7.2',
-        'scipy>=0.9',
-        'astropy>=0.4'
-    ],
+setuptools.setup(
+    name='sparkdl',
+    version=sparkdl.__version__,
+    packages=setuptools.find_packages(exclude=exclude_packages),
+    url="https://github.com/databricks/spark-deep-learning",
+    author="Weichen Xu",
+    author_email="weichen.xu@databricks.com",
+    description="Deep Learning Pipelines for Apache Spark",
+    long_description="",
     classifiers=[
-        'Programming Language :: Python',
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: BSD License',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Astronomy',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
     ],
+    platforms=["Linux"],
+    license="BSD",
+    keywords="spark deep learning horovod model distributed training",
+    install_requires=[],
+    extras_require={},
+    tests_require=["nose", "pytest"],
+    zip_safe=False,
 )
